8902cb020fb0c9d6b389da7415e56036c9ffd9a3
daniely998 <danzone998@gmail.com>
2026-05-20 14:51:38 +0800

    upcoming project details

--- src/models/projects.js
+++ src/models/projects.js
@@ -14,24 +14,69 @@ const getAllProjects = async() => {
 }
 
 const getProjectsByOrganizationId = async (organizationId) => {
-      const query = `
-        SELECT
-          project_id,
-          organization_id,
-          title,
-          description,
-          project_location,
-          project_date
-        FROM project
-        WHERE organization_id = $1
-        ORDER BY project_date;
-      `;
-      
-      const queryParams = [organizationId];
-      const result = await db.query(query, queryParams);
+  const query = `
+    SELECT
+      project_id,
+      organization_id,
+      title,
+      description,
+      project_location,
+      project_date
+    FROM project
+    WHERE organization_id = $1
+    ORDER BY project_date;
+  `;
+  
+  const queryParams = [organizationId];
+  const result = await db.query(query, queryParams);
 
-      return result.rows;
+  return result.rows;
+};
+
+const getUpcomingProjects = async (number_of_projects) => {
+  const query = `
+    SELECT 
+      p.project_id,
+      p.title,
+      p.description,
+      p.project_date,
+      p.project_location,
+      p.organization_id,
+      o.name
+    FROM project p
+    JOIN organization o ON p.organization_id = o.organization_id
+    WHERE p.project_date >= CURRENT_DATE
+    ORDER BY p.project_date ASC
+    LIMIT $1;
+  `;
+
+  const queryParams = [number_of_projects];
+  const result = await db.query(query, queryParams);
+
+  return result.rows;
+};
+
+const getProjectDetails = async (id) => {
+  const query = `
+    SELECT
+      p.project_id,
+      p.title,
+      p.description,
+      p.project_date,
+      p.project_location,
+      p.organization_id,
+      o.name
+    FROM project p
+    JOIN organization o ON p.organization_id = o.organization_id
+    WHERE project_id = $1
+  `;
+  
+  const queryParams = [id];
+  const result = await db.query(query, queryParams);
+
+  // Return the first row of the result set, or null if no rows are found
+  return result.rows.length > 0 ? result.rows[0] : null;
 };
 
 // Export the model functions
-export { getAllProjects, getProjectsByOrganizationId };
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails };
